6a64ecd60d06c3ed94b241b542257af1764642ea
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-11-19 17:16:55 +0330

    add Hotels compo

--- src/commponents/AppLayout/AppLayout.jsx
+++ src/commponents/AppLayout/AppLayout.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 function AppLayout() {
   return (
-    <div className='appLayout'>
-      <div className="sidebar">sidebar</div>
+    <div className="appLayout">
+      <div className="sidebar">
+        <Outlet />
+      </div>
       <div className="mapContainer">map</div>
     </div>
-  )
+  );
 }
 
-export default AppLayout
+export default AppLayout;
