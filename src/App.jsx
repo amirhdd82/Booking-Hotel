ee88107f28c5c5bbd32518cc1462bcb36bedbc64
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-11-19 16:53:44 +0330

    add Route to project

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./commponents/Headers/Header";
 import LocationsList from "./commponents/LocationsList/LocationsList";
 import { Toaster } from "react-hot-toast";
 import { Route, Routes } from "react-router-dom";
+import AppLayout from "./commponents/AppLayout/AppLayout";
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<LocationsList />} />
+        <Route path="/hotels" element={<AppLayout />}></Route>
+        <Route index element={<div>Hotel</div>}></Route>
+        <Route path=":id" element={<div>single hotel</div>}></Route>
       </Routes>
     </div>
   );
