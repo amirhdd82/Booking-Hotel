15d0caca15021dee06e49cea9a14f20e7a9f2f6c
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-11-24 16:56:42 +0330

    add new custom hook

--- src/commponents/Map/Map.jsx
+++ src/commponents/Map/Map.jsx
@@ -22,6 +22,7 @@ function Map() {
         zoom={13}
         scrollWheelZoom={true}
       >
+        <button className="getLocation">Use Your Location</button>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
