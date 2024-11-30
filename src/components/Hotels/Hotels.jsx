8ad9c6889b550a9c3c72f525c00c293d256c7e5b
Amir-Haddadi <112104182+amirhdd82@users.noreply.github.com>
2024-11-30 16:55:20 +0330

    change image size

--- src/components/Hotels/Hotels.jsx
+++ src/components/Hotels/Hotels.jsx
@@ -20,7 +20,7 @@ function Hotels() {
                 item.id === currentHotel?.id ? "current-hotel" : ""
               }`}
             >
-              <img src={item.picture_url.url} alt={item.name} />
+              <img src={item.xl_picture_url.url} alt={item.name} />
               <div className="searchItemDesc">
                 <p className="location">{item.smart_location}</p>
                 <p className="name">{item.name}</p>
